e1128d135c51e24fc5d40364b848f6a57d6b1d50
nikabezhashvili <nikabezhshvili0@gmail.com>
2025-12-01 00:06:14 +0400

    second commit

---
+++ level 018/classwork/level018.py
@@ -0,0 +1,43 @@
+
+
+# 1
+for i in range(0,100):
+    print(i)
+
+# 2
+for i in range(0,101):
+    print(i)
+    
+# 3
+for i in range(10,20):
+    print(i)
+
+# 4
+for i in range(100,201,5):
+    print(i)
+
+# 5
+for i in range(10,-1,-1):
+    print(i)
+
+# 6
+num = 1
+
+while num < 11:
+    print(num)
+    num += 1
+
+# 7
+num = 0
+
+while num < 101:
+    print(num)
+    num += 2
+
+
+num = 10
+
+while num > -1:
+    print(num)
+    num -= 1
+
---
+++ level 018/homework/level 018.py
@@ -0,0 +1,94 @@
+# for loop
+# 1
+for i in range(0,39):
+    print(i)
+# 2
+for i in range(30,40):
+    print(i)
+# 3
+for i in range(12,120):
+    print(i)
+# 4
+for i in range(18):
+    print('nika')
+    print(14)
+# 5
+for i in range(10,100,3):
+    print(i)
+
+# 6
+for i in range(1, 31):
+    print(i, "ვაშლი")
+
+# 7
+age = input("შემოიყვანეთ თქვენი ასაკი: ")
+
+for i in range(13):
+    print(age)
+
+# while loop
+
+# 8
+name = "nika"
+i = 1
+while i <= 10:
+    print(name)
+    i = i + 1
+
+# 9
+i = 0
+while i <24:
+    print(i)
+    i = i+1
+
+# 10
+# გავაკეთებ ხუთ for loop და ხუთ while loop
+
+# for loop
+for i in range(10):
+    print(i)
+
+for i in range(3,20):
+    print(i)
+
+for i in range(5,30,5):
+    print(i)
+
+for i in range(20):
+    print('ნიკა')
+
+name = 'nika'
+for i in name:
+    print(i)
+
+# while loop
+i = 0
+while i < 10:
+    print(i)
+    i +=1
+
+i = 5 
+while i < 21:
+    print(i)
+    i +=5
+
+age = 14
+
+i = 3
+while i <= 15:
+    print(age)
+    i = i + 2
+
+num = 15
+
+while num > 5:
+    print(num)
+    num -= 2
+
+
+last_name = 'bezhashvili'
+
+i = 5
+while i < 16:
+    print(last_name)
+    i = i + 5
