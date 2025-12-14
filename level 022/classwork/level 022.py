ae4e399d9fba876a46b33c10c3a88da030e0f409
nikabezhashvili <nikabezhshvili0@gmail.com>
2025-12-15 01:38:53 +0400

    second commit

---
+++ level 022/classwork/level 022.py
@@ -0,0 +1,27 @@
+# 1
+arr = ['mercedes','bmw','audi','ferari','opel']
+print(arr[0], arr[2])
+
+# 2
+arr = [2 , 4 , 6 , 8 , 10]
+print(arr[1],arr[3])
+
+# 3
+arr = [1,2,3,4,5]
+print(arr[3])
+
+# 4
+arr = [1,2,3,4,5,6,7,8,9]
+arr[2] = 'nika'
+print(arr)
+
+# 5
+arr = [4,5,6]
+sum = arr[0] + arr[2]
+print(sum)
+
+# 6
+arr = [1,2,3,4,5,6,7]
+arr[0] = 'nika'
+arr[4] = 'რაღაცა' 
+print(arr)
