b5f9266aae26e92c84c19e712d137d24d351f513
nikabezhashvili <nikabezhshvili0@gmail.com>
2026-05-29 20:27:53 +0400

    second commit

---
+++ level 050/homework/script.js
@@ -0,0 +1,33 @@
+// 1
+console.log(Math.random() * 10);
+
+// 2
+console.log(Math.floor(5.99));
+
+// 3
+console.log(Math.ceil(4.1));
+
+// 4
+console.log(Math.round(2.4));
+console.log(Math.round(2.5));
+
+// 5
+console.log(Math.sign(-10)); // რავი ნებისმიერი ნეგატიური რიცხვი მაგალითად დავწერე -10
+console.log(Math.sign(0)); // აქ მაგალითისთვის დავწერე 0
+console.log(Math.sign(10)); // აქ მაგალითისთვის დავწერე 10
+
+// 6
+console.log(Math.pow(5, 3));
+
+// 7
+console.log(Math.max(12, 45, 7, 89, 23));
+
+// 8
+console.log(Math.min(18, 25, 14));
+
+// 9
+console.log(Math.sqrt(3 ** 2 + 4 ** 2));
+
+// 10
+console.log(Number.isInteger(5));
+console.log(Number.isInteger(5.5));
